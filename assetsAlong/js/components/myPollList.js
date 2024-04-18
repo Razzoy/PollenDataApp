261bcffe435ac695a603e93bb253df1e98091c23
Razzoy <1081892@ucn.dk>
2024-04-18 15:02:20 +0200

    codealong v2

--- assetsAlong/js/components/myPollList.js
+++ assetsAlong/js/components/myPollList.js
@@ -1,4 +1,5 @@
 import { myFetch } from "../Utils/apiUtils.js"
+import { getFriendlyPollNames, getPollImage } from "../Utils/customUtils.js"
 
 export const myPollList = async () => {
     const endpoint = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=52.52&longitude=13.41&current=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&hourly=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&timezone=Europe%2FBerlin&forecast_days=1`
@@ -13,10 +14,12 @@ export const myPollList = async () => {
     arrPolls.map(value => {
         const li = document.createElement('li')
         const span_image = document.createElement('span')
-        span_image.innerText = '';
+        const img = document.createElement('img')
+        img.src = `./assetsAlong/images/${getPollImage(value)}`
+        span_image.appendChild(img);
 
         const span_name = document.createElement('span')
-        span_name.innerText = value;
+        span_name.innerText = getFriendlyPollNames(value);
 
         const span_number = document.createElement('span')
         span_number.innerText = current[value];
